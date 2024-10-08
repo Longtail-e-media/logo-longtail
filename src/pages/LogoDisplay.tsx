cb195bbd0750fd5a0297d6bbdefaf325f10af704
Purna Shrestha <crush9918@gmail.com>
2024-10-08 22:26:31 +0545

    Fix: Dynamic API

--- src/pages/LogoDisplay.tsx
+++ src/pages/LogoDisplay.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Logo } from '../interfaces/types';
-import { logoDetails } from '../constants/data';
 
 interface LogoDisplayProps {
   logos: Logo[];
@@ -14,13 +13,27 @@ const LogoDisplay: React.FC<LogoDisplayProps> = ({ logos }) => {
     setVisibleItems((prevVisibleItems) => prevVisibleItems + 24);
   };
 
-  if (!logos || logos.length === 0) return <p>loading...</p>;
+  if (!logos || logos.length === 0)
+    return (
+      <section className="flex h-screen w-full flex-col items-center justify-center gap-8">
+        <p className="text-center text-base md:text-4xl">No logos found.</p>
+        <Link
+          to="/"
+          className="group inline-flex items-center justify-center gap-2 rounded-full bg-[#bf1f49] px-6 py-1 text-xs font-bold text-white md:text-base"
+        >
+          <i className="text-base transition-all duration-300 group-hover:-translate-x-2 md:text-2xl">
+            &larr;
+          </i>
+          Back to Home
+        </Link>
+      </section>
+    );
 
   return (
     <>
       <main>
         <section className="grid grid-cols-2 gap-4 md:grid-cols-6">
-          {logoDetails.slice(0, visibleItems).map((logo: Logo, index) => (
+          {logos.slice(0, visibleItems).map((logo: Logo, index) => (
             <Link
               to={`/logo/${logo.name}`}
               key={index}
@@ -36,7 +49,7 @@ const LogoDisplay: React.FC<LogoDisplayProps> = ({ logos }) => {
           ))}
         </section>
 
-        {logoDetails.length > visibleItems && (
+        {logos.length > visibleItems && (
           <div className="mt-14 flex items-center justify-center">
             <button
               onClick={handleLoadMore}
