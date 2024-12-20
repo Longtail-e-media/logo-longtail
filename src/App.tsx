f41d2690aa60ffce0822da39fb55b16d6bc19954
Purna Shrestha <crush9918@gmail.com>
2024-12-20 22:27:25 +0545

    fix: The production skeleton issue

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,7 @@ const App: React.FC = () => {
   if (error) return <p>Error: {error.message}</p>;
 
   return (
-    <Router basename='/logo'>
+    <Router basename='/logo/'>
       <Navbar
         isAdmin={isAdmin}
         setIsAdmin={setIsAdmin}
--- src/pages/LogoDisplay.tsx
+++ src/pages/LogoDisplay.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Logo } from '../interfaces/types';
 import Error404 from '../layouts/Error404';
@@ -11,38 +11,18 @@ interface LogoDisplayProps {
 
 const LogoDisplay: React.FC<LogoDisplayProps> = ({ logos, isAdmin }) => {
   const [visibleItems, setVisibleItems] = useState(36);
-  const [loadingStates, setLoadingStates] = useState<Record<string, boolean>>(
-    {},
-  );
-
-  useEffect(() => {
-    // Initialize loading states for logos that don't exist in the current state
-    const newLoadingStates: Record<string, boolean> = {};
-    logos.forEach((logo) => {
-      if (!loadingStates.hasOwnProperty(logo.name)) {
-        newLoadingStates[logo.name] = true;
-      }
-    });
-    if (Object.keys(newLoadingStates).length > 0) {
-      setLoadingStates((prevStates) => ({
-        ...prevStates,
-        ...newLoadingStates,
-      }));
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [logos]);
+  const [loadingLogos, setLoadingLogos] = useState<Record<string, boolean>>({});
 
   const handleLoadMore = () => {
     setVisibleItems((prevVisibleItems) => prevVisibleItems + 24);
   };
 
   const handleImageLoad = (logoName: string) => {
-    setLoadingStates((prevStates) => ({
-      ...prevStates,
-      [logoName]: false,
+    setLoadingLogos((prevState) => ({
+      ...prevState,
+      [logoName]: false, // Mark the image as loaded
     }));
   };
-
   if (!logos || logos.length === 0) {
     return <Error404 displayText="No Logo Found." />;
   }
@@ -53,17 +33,15 @@ const LogoDisplay: React.FC<LogoDisplayProps> = ({ logos, isAdmin }) => {
         <section className="grid grid-cols-2 gap-4 md:grid-cols-4 xl:grid-cols-6">
           {logos.slice(0, visibleItems).map((logo: Logo) => (
             <div key={logo.name} className="relative">
-              {loadingStates[logo.name] && <SkeletonLoader />}
+              {loadingLogos[logo.name] !== false && <SkeletonLoader />}
               <Link
                 to={`/logo/${logo.name}`}
                 className={`flex items-center justify-center border bg-white p-4 text-center shadow-sm transition-all duration-100 hover:shadow md:px-4 md:py-8 ${
-                  loadingStates[logo.name] ? 'hidden' : 'block'
+                  loadingLogos[logo.name] === false ? '' : 'hidden'
                 }`}
               >
                 <img
-                  // src={logo.img_thumb}
-                  // src={logo.logoFormats.img_svg}
-                  src={isAdmin ? logo.img_thumb : logo.img_thumb}
+                  src={isAdmin ? logo.logoFormats.img_svg : logo.img_thumb}
                   alt={logo.title}
                   className="aspect-square size-36 bg-transparent object-contain md:size-48"
                   draggable="false"
